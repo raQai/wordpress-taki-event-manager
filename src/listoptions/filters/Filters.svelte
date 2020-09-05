40061d2f5f66e789f8a1cb9e62d12bd1849ac7c1
Patrick Bogdan <raQai@users.noreply.github.com>
2020-09-05 10:49:51 +0200

    [SelectFilter] Implement first stage of filter components.

---
+++ src/listoptions/filters/Filters.svelte
@@ -0,0 +1,57 @@
+<script>
+  import SelectFilter from "./SelectFilter.svelte";
+
+  export let filterSettings = [];
+  export let selectEventCallback = ({ filterSettings: filterSettings }) => {};
+</script>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    opacity: 0.7;
+    margin:0;
+  }
+  .filter {
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    margin: 0 0 1rem;
+  }
+  .select {
+    margin-right:-1.1rem;
+  }
+  .select::after {
+    content: "\25BC";
+    position: relative;
+    right: 2.1rem;
+    top: 0.7rem;
+    font-size: 1.2rem;
+  }
+  @media (min-width: 600px) {
+    .wrapper {
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+    .filter {
+      flex-basis: 40%;
+      flex-grow: 1;
+    }
+  }
+  @media (min-width: 1000px) {
+    .filter {
+      flex-basis: auto;
+      flex-grow: 0;
+    }
+  }
+</style>
+
+<div class="wrapper">
+  {#each filterSettings as filterItem}
+    {#if filterItem.type === 'select'}
+      <div class="filter select">
+        <SelectFilter selectFilterSettings={filterItem} />
+      </div>
+    {/if}
+  {/each}
+</div>
---
+++ src/listoptions/filters/SelectFilter.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let selectFilterSettings = {
+    type: "select",
+    label: "label",
+    items: new Map(), // items in the select filter as key->value pairs
+    selected,
+  };
+</script>
+
+<style>
+  label {
+    flex-shrink: 0;
+    margin: 0 1rem 0 0;
+    font-size: 1.4rem;
+    line-height: 3.2rem;
+  }
+  select {
+    flex-grow: 1;
+    height: 3.2rem;
+    width: 100%;
+    font-size: 1.2rem;
+    border: none;
+    padding: 0.7rem;
+    background-color: var(--taki-grey1);
+    -moz-appearance: none;
+    appearance: none;
+    cursor:pointer;
+  }
+  option {
+    color: #222;
+  }
+  @media (min-width: 600px) {
+    label {
+      margin: 0 1rem;
+    }
+  }
+</style>
+
+{#if selectFilterSettings.items}
+  <label
+    for="select-{selectFilterSettings.label
+      .toString()
+      .toLocaleLowerCase()}">{selectFilterSettings.label.toString()}</label>
+  <select id="select-{selectFilterSettings.label.toString().toLowerCase()}">
+    <option
+      style="color:#aaa"
+      selected={!selectFilterSettings.items.has(selectFilterSettings.select)}>
+      --- Alle ---
+    </option>
+    {#each [...selectFilterSettings.items.keys()] as key}
+      <option selected={key == selectFilterSettings.selected} {key}>
+        {selectFilterSettings.items.get(key)}
+      </option>
+    {/each}
+  </select>
+{/if}
