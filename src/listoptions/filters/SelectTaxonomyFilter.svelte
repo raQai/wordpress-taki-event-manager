b0705779a69f7ac979e41bd5e0bd8b5788caffea
Patrick Bogdan <raQai@users.noreply.github.com>
2020-09-14 10:26:37 +0200

    [SelectTaxonomyFilter] Refactor remove unnecessary code.

--- src/listoptions/filters/SelectTaxonomyFilter.svelte
+++ src/listoptions/filters/SelectTaxonomyFilter.svelte
@@ -31,10 +31,6 @@
     line-height: 3.2rem;
     margin-right: -1rem;
   }
-  .select {
-    width: 100%;
-    position: relative;
-  }
   select {
     width: 100%;
     height: 3.2rem;
@@ -46,15 +42,6 @@
     appearance: none;
     cursor: pointer;
   }
-  .select::after {
-    content: "\25BC";
-    position: absolute;
-    font-size: 1.2rem;
-    top: 0.75rem;
-    right: 1.3rem;
-    pointer-events: none;
-    opacity: 0.6;
-  }
   option {
     color: #222;
   }
@@ -62,16 +49,14 @@
   }
 </style>
 
-{#if values}
+{#if Array.isArray(values) && values.length}
   <label for="select-{taxonomy}">{label}</label>
-  <div class="select">
-    <select id="select-{taxonomy}" bind:value={selected}>
-      <option style="color:#aaa" value="">--- Alle ---</option>
-      {#each values as value}
-        {#if value && value.slug && value.name}
-          <option value={value.slug}>{value.name}</option>
-        {/if}
-      {/each}
-    </select>
-  </div>
+  <select id="select-{taxonomy}" bind:value={selected}>
+    <option style="color:#aaa" value="">--- Alle ---</option>
+    {#each values as value}
+      {#if value && value.slug && value.name}
+        <option value={value.slug}>{value.name}</option>
+      {/if}
+    {/each}
+  </select>
 {/if}
