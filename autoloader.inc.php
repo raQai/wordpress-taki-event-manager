3d3973577049942c6b39de34d2b004616adb8fa5
Patrick Bogdan <raQai@users.noreply.github.com>
2020-10-10 20:09:54 +0200

    [autoloader] Add php doc.

--- autoloader.inc.php
+++ autoloader.inc.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Copyright Patrick Bogdan. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+/**
+ * Autoloader for this plugin
+ *
+ * @since     1.0.0
+ *
+ * @package   BIWS\TaKiEventManager
+ *
+ * @author     Patrick Bogdan
+ * @copyright  2020 Patrick Bogdan
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 or later
+ */
+
 namespace BIWS\TaKiEventManager;
 
 defined('ABSPATH') or die('Nope!');
